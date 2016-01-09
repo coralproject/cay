5d310d27c10d607fe32f91f018e9672a5f2ee72a
Colin Megill <colinmegill@gmail.com>
2016-01-08 22:17:57 -0800

    line graph working, moving to abstractions

--- src/components/data-explorer-visualization.js
+++ src/components/data-explorer-visualization.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Radium from 'radium';
-import { VictoryChart, VictoryAxis, VictoryBar } from 'victory';
+import { VictoryChart, VictoryAxis, VictoryBar, VictoryLine, VictoryScatter } from 'victory';
 import settings from '../settings';
-
+import _ from "lodash";
+import d3 from "d3";
 
 @Radium
 class DataExplorerVisualization extends React.Component {
 
-  getVictoryComponentByType() {
+  getBarVictoryComponent() {
     return (
       <VictoryBar
         style={{data:
           {width: 1, fill: "orange"}
         }}
         data={this.props.dataset}/>
+    )
+  }
+  getLineVictoryComponent() {
+    return (
+      <VictoryLine
+        padding={75}
+        data={this.props.dataset}
+        interpolation="monotone"
+        style={{
+          data: {
+            strokeWidth: 1,
+            ":hover": {stroke: "#c33b33"}
+          },
+          labels: {fontSize: 12}
+        }}/>
     )
   }
   render() {
@@ -26,33 +42,31 @@ class DataExplorerVisualization extends React.Component {
           top: 75,
           bottom: 140,
           left: 70,
-          right: 40
+          right: 70
         }}
+        scale={{x: d3.time.scale(), y: d3.scale.linear()}}
         domainPadding={{x: 20}}>
         <VictoryAxis
           style={{
             axis: {stroke: "gray"},
             ticks: {stroke: "gray"},
+
             tickLabels: {
               fontSize: 10,
-              transform: `rotate(45deg) translate(30px, 0px)`,
             },
             axisLabels: {
               fontsize: 16,
             }
           }}
-          label="commenter id" />
+          tickCount={10}
+          label={this.props.independentVariableName} />
         <VictoryAxis dependentAxis
-          label="# of comments"
+          label={this.props.dependentVariableName}
           style={{
-            grid: {
-              stroke: "lightgrey",
-              strokeWidth: 1
-            },
             axis: {stroke: "gray"},
             ticks: {stroke: "transparent"}
           }}/>
-        {this.getVictoryComponentByType()}
+        {this.getLineVictoryComponent()}
       </VictoryChart>
       </div>
     );
@@ -60,3 +74,28 @@ class DataExplorerVisualization extends React.Component {
 }
 
 export default DataExplorerVisualization;
+
+
+      // <svg
+      //   height={600}
+      //   width={800}>
+      //   <VictoryAxis
+      //     style={{
+      //       axis: {stroke: "gray"},
+      //       ticks: {stroke: "transparent"},
+      //       tickLabels: {
+      //         fontSize: 6,
+      //         transform: `rotate(45deg) translate(10px, 0px)`,
+      //       },
+      //       axisLabels: {
+      //         fontsize: 16,
+      //       }
+      //     }}
+      //     tickCount={10}
+      //     tickFormat={(x) => x.getFullYear()}
+      //     scale={d3.time.scale()}
+      //     label={this.props.independentVariableName} />
+      //   <VictoryAxis dependentAxis
+      //     label={this.props.dependentVariableName}/>
+      //   {this.getLineVictoryComponent()}
+      // </svg>
--- src/containers/data-explorer.js
+++ src/containers/data-explorer.js
@@ -6,23 +6,36 @@ import settings from '../settings';
 import { fetchDataExplorationDataset } from '../actions';
 import Page from './page';
 import DataExplorerVisualization from "../components/data-explorer-visualization";
+import moment from "moment";
 
 @connect(state => state.dataExplorer)
 @Radium
 class DataExplorer extends React.Component {
 
   componentWillMount() {
-    this.props.dispatch(fetchDataExplorationDataset("top_commenters_by_count", null));
-    // this.props.dispatch(fetchDataExplorationDataset("comments_per_day", {
-    //   start_date: "2014-01-01",
-    //   end_date: "2015-01-01"
-    // }))
-    // field was top_commenters_by_count w/ no query string
-    // http://localhost:4000/comments_per_day/exec?start_date=2014-01-01&end_date=2015-01-01
+    // this.props.dispatch(fetchDataExplorationDataset("top_commenters_by_count", null));
+    this.props.dispatch(fetchDataExplorationDataset("comments_per_day", {
+      start_date: "2014-01-01",
+      end_date: "2015-01-01"
+    }))
   }
 
-  parseDataFromXenia() {
+  parseCommentsPerDay() {
+    const victoryFormat = this.props.dataset.map((item) => {
+      const date = item._id
+      return {
+        x: new Date(
+          date.year,
+          date.month,
+          date.day
+        ),
+        y: item.comments
+      }
+    })
+    return victoryFormat;
+  }
 
+  parseTopCommentersByCount() {
     const victoryFormat = this.props.dataset.map((item) => {
       return {
         x: item._id.user_id + "" , // hack - cast to string for now for graph spacing
@@ -38,7 +51,10 @@ class DataExplorer extends React.Component {
         <h1>Data Explorer</h1>
           {
             this.props.dataset ?
-              <DataExplorerVisualization dataset={this.parseDataFromXenia.call(this)}/> :
+              <DataExplorerVisualization
+                independentVariableName={"indVarName"}
+                dependentVariableName={"depVarName"}
+                dataset={this.parseCommentsPerDay.call(this)}/> :
               "Spinner"
           }
       </Page>

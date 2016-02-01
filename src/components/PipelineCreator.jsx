e785bb27dab266846f5660bd5cf8aa75417434e3
Riley Davis <riley.davis@gmail.com>
2016-02-01 13:32:57 -0700

    show/hide UI components based on selected breakdown

--- src/components/PipelineCreator.jsx
+++ src/components/PipelineCreator.jsx
@@ -7,8 +7,67 @@ import Select from 'react-select';
 
 @Radium
 class PipelineCreator extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedBreakdown: 'total', // asset|author|section|user|total
+      resultFields: [], // what fields the user is interested in measuring; replies, replies per comment, etc
+      specificBreakdowns: [], // this could be particular author(s), or a specific section
+      // hard-code the date range of the NYT dataset.
+      minDate: '2003-05-13',
+      maxDate: '2015-01-01',
+      computedQuery: null
+    };
+  }
+
   handleCreatePipeline() {
     console.log(this.refs);
+
+    /* this will be moved to actions, construct and pass an object from here */
+    var computedQuery = {
+      name: 'custom_query',
+      desc: 'Returns a time series of comments bounded by iso dates!  Totals or broken down by duration [day|week|month] and target type total|user|asset|author|section]',
+      pre_script: '',
+      pst_script: '',
+      params: [],
+      queries: [
+        {
+          name: 'custom_query',
+          type: 'pipeline',
+          collection: 'comment_timeseries',
+          commands: [
+            {
+              $match: {
+                start_iso: {$gte: '#date:2013-01-01'}
+              }
+            },
+            {
+              $match: {
+                start_iso: {$lt: '#date:2014-12-31'}
+              }
+            },
+            {
+              $match: {duration: 'week'}
+            },
+            {
+              $match: {target: 'author'}
+            },
+            {$match: {target_doc: 'Paul Krugman'}},
+            {
+              $sort: {
+                start_iso: 1
+              }
+            }
+          ],
+          return: true
+        }
+      ],
+      enabled: true
+    };
+
+    this.props.dispatch(createPipelineValueChanged(computedQuery));
   }
 
   getTargets(target) {
@@ -19,88 +78,84 @@ class PipelineCreator extends React.Component {
     });
   }
 
-  updateOutput(author) {
-    console.log('updateOutput', author);
+  getSpecific(selectedBreakdown) {
+    if (selectedBreakdown === 'asset') {
+      // ????
+    } else if (selectedBreakdown === 'author') {
+      // return a list of authors
+      return (
+        <div>
+          <p style={{marginBottom: 10, marginTop: 10}}>on assets written by</p>
+          <Select
+            options={this.getTargets('author')}
+            name="selected-targets"
+            placeholder="Author"
+            multi={true} />
+          </div>
+        );
+    } else if (selectedBreakdown === 'section') {
+      return (
+        <div>
+          <p style={{marginBottom: 10, marginTop: 10}}>specifically, these sections</p>
+          <Select
+            options={this.getTargets('section')}
+            name="selected-targets"
+            placeholder="Section"
+            multi={true} />
+          </div>
+        );
+    } else if (selectedBreakdown === 'user') {
+      return (<p>Auto-complete widget yo</p>);
+    } else {
+      return '';
+    }
+  }
 
-    /* this will be moved to actions, construct and pass an object from here */
-    var computedQuery = {
-      "name": "custom_query",
-      "desc": "Returns a time series of comments bounded by iso dates!  Totals or broken down by duration [day|week|month] and target type total|user|asset|author|section]",
-      "pre_script": "",
-      "pst_script": "",
-      "params": [],
-      "queries": [
-        {
-          "name": "custom_query",
-          "type": "pipeline",
-          "collection": "comment_timeseries",
-          "commands": [
-            {
-              "$match": {
-                "start_iso": {
-                  "$gte": "#date:2013-01-01"
-                }
-              }
-            },
-            {
-              "$match": {
-                "start_iso": {
-                  "$lt": "#date:2014-12-31"
-                }
-              }
-            },
-            {
-              "$match": {
-                "duration": "week"
-              }
-            },
-            {
-              "$match": {
-                "target": "author"
-              }
-            },
-            {"$match": {"target_doc": author}},
-            {
-              "$sort": {
-                "start_iso": 1
-              }
-            }
-          ],
-          "return": true
-        }
-      ],
-      "enabled": true
-    };
+  // this toggles the UI to load more specific options
+  updateOutput(breakdown) {
+    this.setState({selectedBreakdown: breakdown});
+  }
 
-    this.props.dispatch(createPipelineValueChanged(computedQuery));
+  getBreakdownOptions() {
+    var possible = ['total', 'user', 'asset', 'section', 'author'];
+
+    return possible.map(p => {
+      return {value: p, label: p};
+    });
   }
 
   getFieldOptions() {
 
     return [
       {
-        value: "comments",
-        label: "comments (total, accepted, rejected & escalated)",
+        value: 'comments',
+        label: 'comments (total, accepted, rejected & escalated)'
       },
       {
-        value: "replies",
-        label: "replies (total)",
+        value: 'replies',
+        label: 'replies (total)'
       },
       {
-        value: "replies_per_comment",
-        label: "replies (per comment)",
+        value: 'replies_per_comment',
+        label: 'replies (per comment)'
       },
       {
-        value: "accept_ratio",
-        label: "accept ratio",
-      },
-    ]
+        value: 'accept_ratio',
+        label: 'accept ratio'
+      }
+    ];
   }
 
   render() {
 
     return (
       <div>
+          <p>I want to know about</p>
+          <Select
+            options={this.getBreakdownOptions()}
+            name="breakdown-type"
+            onChange={this.updateOutput.bind(this)} />
+
           <p style={{marginBottom: 10}}>
             {"Show me:"}
           </p>
@@ -112,20 +167,12 @@ class PipelineCreator extends React.Component {
             options={this.getFieldOptions()}
             placeholder="comments / replies / accept ratio"/>
 
-          <p style={{marginBottom: 10, marginTop: 10}}> {"on assets written by"} </p>
-          <div>
-            <Select
-              options={this.getTargets('author')}
-              name="selected-targets"
-              placeholder="Author"
-              multi={true}
-              onChange={this.updateOutput.bind(this)} />
-          </div>
+          {this.getSpecific(this.state.selectedBreakdown)}
 
           <p style={{marginBottom: 10, marginTop: 10}}>between</p>
-          <input type="date" ref="date_start" />
+          <input type="date" ref="date_start" value={this.state.minDate} />
           <p style={{marginBottom: 10, marginTop: 10}}>and</p>
-          <input type="date" ref="date_end" />
+          <input type="date" ref="date_end" value={this.state.maxDate} />
         <p style={{marginTop: 10}}> + Add another question for comparison </p>
         <div style={{marginTop: 20}}>
           <button onClick={this.handleCreatePipeline.bind(this)}> Create </button>

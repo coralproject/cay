e90f1bf5a6f306ad9beeb1c7a713eff1728d0c47
Riley Davis <riley.davis@gmail.com>
2016-02-22 11:07:43 -0700

    update build file to move favicons

--- post-build.sh
+++ post-build.sh
@@ -3,3 +3,6 @@
 cp ./index.html ./dist/index.html
 cp -r ./public/img ./dist/img
 cp ./public/config.json ./dist/config.json
+cp ./public/favicon.ico ./dist/favicon.ico
+cp ./public/browserconfig.xml ./dist/browserconfig.xml
+cp ./public/manifest.json ./dist/manifest.json

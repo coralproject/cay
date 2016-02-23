e637319a6c6100fe6c9c114385e74978cb0cb6dd
Riley Davis <riley.davis@gmail.com>
2016-02-23 11:01:54 -0700

    reverting regression

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -4,7 +4,7 @@ import Radium from 'radium';
 import Searchbar from '../../forms/Searchbar';
 
 // import {MdMenu} from 'react-icons';
-// import MdMenu from 'react-icons/lib/md/menu';
+import MdMenu from 'react-icons/lib/md/menu';
 
 @Radium
 class Header extends React.Component {
@@ -16,8 +16,7 @@ class Header extends React.Component {
   render() {
     return (
       <nav style={styles.nav}>
-        <p onClick={this.handleClick.bind(this)}>M</p>
-        {/*<MdMenu style={styles.sidebarToggle} onClick={this.handleClick.bind(this)} />*/}
+        <MdMenu style={styles.sidebarToggle} onClick={this.handleClick.bind(this)} />
         {/*<Searchbar style={styles.searchbar}/>*/}
       </nav>
     );

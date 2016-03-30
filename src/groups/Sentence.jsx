3e242c95ad82f077e19d0d634bc105b76653701b
Colin Megill <colinmegill@gmail.com>
2016-03-30 13:39:27 -0700

    add d3 colors as per riley suggestion

--- src/groups/Sentence.jsx
+++ src/groups/Sentence.jsx
@@ -62,6 +62,22 @@ class Sentence extends React.Component {
     return breakdown;
   }
 
+  randomColor(i) {
+    const flatColors = [
+      '#1f77b4',
+      '#ff7f0e',
+      '#2ca02c',
+      '#d62728',
+      '#9467bd',
+      '#8c564b',
+      '#e377c2',
+      '#7f7f7f',
+      '#bcbd22',
+      '#17becf'
+    ];
+    return flatColors[i];
+  }
+
   getFilters() {
     let nonDefaultFilters = [];
     let indexOfLastNonDefault;

89a4b8b76058049c27271b5e237422490863c28e
Colin Megill <colinmegill@gmail.com>
2016-06-08 08:55:14 -0700

    Only one sidebar item now, so always active for now

--- src/app/layout/sidebar/MenuItem.jsx
+++ src/app/layout/sidebar/MenuItem.jsx
@@ -12,7 +12,7 @@ class MenuItem extends React.Component {
   render() {
     return (
       <li style={styles.base}>
-        <RadiumLink style={styles.link} to={this.props.target} activeStyle={styles.base[':hover']}>
+        <RadiumLink style={styles.temporaryAlwaysActiveLinkWhileOnlyOneItem} to={this.props.target} activeStyle={styles.base[':hover']}>
           <span style={ styles.icon }>{this.props.icon}</span>
         </RadiumLink>
       </li>
@@ -28,15 +28,23 @@ const styles = {
       color: settings.brandColor,
     },
   },
-  link: {
-    padding: 20,
-    display: 'block',
-    color: "white",
-    textDecoration: 'none',
-    ':hover': {
+  // link: {
+  //   padding: 20,
+  //   display: 'block',
+  //   color: "white",
+  //   textDecoration: 'none',
+  //   ':hover': {
+  //     backgroundColor: "white",
+  //     color: settings.brandColor,
+  //   },
+  // },
+  temporaryAlwaysActiveLinkWhileOnlyOneItem: {
+      padding: 20,
+      display: 'block',
+      color: "white",
+      textDecoration: 'none',
       backgroundColor: "white",
       color: settings.brandColor,
-    },
   },
   icon: {
     textAlign: 'center',

c2bcbd50ac7b6d05e32890a3af3428ac14f91826
Riley Davis <riley.davis@gmail.com>
2016-07-18 15:15:40 -0600

    rename SubmissionGallery > GalleryManager

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ import SearchEditor from 'app/SearchEditor';
 import NoMatch from 'app/NoMatch';
 import About from 'app/About';
 import SubmissionList from 'app/SubmissionList';
-import SubmissionGallery from 'app/SubmissionGallery';
+import GalleryManager from 'app/GalleryManager';
 
 // Utils
 //import registerServiceWorker from 'serviceworker!./sw.js';
@@ -111,7 +111,7 @@ class Root extends React.Component {
             <Route path="forms/create" component={FormCreate}/>
             <Route path="forms/:id" component={FormEdit}/>
             <Route path="forms/:id/submissions" component={SubmissionList}/>
-            <Route path="forms/:id/gallery" component={SubmissionGallery}/>
+            <Route path="forms/:id/gallery" component={GalleryManager}/>
           </div>
         ) : null}
         <Route path="*" component={NoMatch} />

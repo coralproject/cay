de8d702721b2f4d2f149e6100946d464c3a431b2
Pablo Cubico <pablocuadrado@gmail.com>
2016-07-18 23:39:39 -0300

    Focusing newly created option on multiple choice

--- src/forms/editors/MultipleChoiceEditor.jsx
+++ src/forms/editors/MultipleChoiceEditor.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import Radium from 'radium';
 import { connect } from 'react-redux';
 
@@ -21,6 +21,7 @@ export default class MultipleChoiceEditor extends Component {
       :
         [ { title: 'Option 1' } ]
     };
+    this.focusNew = false;
   }
 
   updateFieldOptions(options) {
@@ -30,9 +31,11 @@ export default class MultipleChoiceEditor extends Component {
     this.props.onEditorChange(updatedField);
   }
 
-  addOption(i) {
+  addOption() {
     var optionsCopy = this.state.options.slice();
     optionsCopy.push({ title: 'Option ' + (this.state.options.length + 1) });
+    // We are not using state on this, it's going to be manipulated on render
+    this.focusNew = true;
     this.setState({ options: optionsCopy });
     this.updateFieldOptions(optionsCopy);
   }
@@ -97,24 +100,35 @@ export default class MultipleChoiceEditor extends Component {
         <div style={ styles.options }>
           {
             this.state.options.map((option, i) => {
-                return (
-                  <div key={ i } style={ styles.optionRow }>
-                    <div style={ styles.optionRowText }>
-                      <input style={ styles.optionInput } type="text" value={ option.title } onChange={ this.updateOption.bind(this, i) } />
-                    </div>
-                    <div style={ styles.optionRowButtons }>
-                      <button style={ styles.optionButton } onClick={ this.duplicateOption.bind(this, i) }><FaCopy /></button>
-                      {
-                        (i > 0) || (i == 0 && this.state.options.length > 1) ?
-                          <button style={ styles.optionButton } onClick={ this.removeOption.bind(this, i) }><FaTrashO /></button>
-                        :
-                          <button style={ styles.optionButton } disabled><FaTrashO /></button>
+              return (
+                <div key={ i } style={ styles.optionRow }>
+                  <div style={ styles.optionRowText }>
+                    <input
+                      ref={
+                        (el) => {
+                          if (el) {
+                            if (this.focusNew && i == this.state.options.length - 1) {
+                              el.focus();
+                              this.focusNew = false;
+                            }
+                          }
+                        }
                       }
-                      <button disabled={ i == 0 } style={ styles.optionButton } onClick={ this.moveOption.bind(this, i, 'up') }><FaArrowUp /></button>
-                      <button disabled={ i == this.state.options.length - 1 } style={ styles.optionButton } onClick={ this.moveOption.bind(this, i, 'down') }><FaArrowDown /></button>
-                    </div>
+                      style={ styles.optionInput } type="text" value={ option.title } onChange={ this.updateOption.bind(this, i) } />
                   </div>
-                )
+                  <div style={ styles.optionRowButtons }>
+                    <button style={ styles.optionButton } onClick={ this.duplicateOption.bind(this, i) }><FaCopy /></button>
+                    {
+                      (i > 0) || (i == 0 && this.state.options.length > 1) ?
+                        <button style={ styles.optionButton } onClick={ this.removeOption.bind(this, i) }><FaTrashO /></button>
+                      :
+                        <button style={ styles.optionButton } disabled><FaTrashO /></button>
+                    }
+                    <button disabled={ i == 0 } style={ styles.optionButton } onClick={ this.moveOption.bind(this, i, 'up') }><FaArrowUp /></button>
+                    <button disabled={ i == this.state.options.length - 1 } style={ styles.optionButton } onClick={ this.moveOption.bind(this, i, 'down') }><FaArrowDown /></button>
+                  </div>
+                </div>
+              );
             })
           }
 

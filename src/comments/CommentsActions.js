cb7fe0219ef1e8add68baf05a5e22010d13a95ec
Riley Davis <riley.davis@gmail.com>
2016-07-20 13:56:23 -0600

    remove dependency on saved xenia query

--- src/comments/CommentsActions.js
+++ src/comments/CommentsActions.js
@@ -30,12 +30,23 @@ export const receiveCommentsFailure = (err) => {
 
 /* xenia_package */
 export const fetchCommentsByUser = (user_id) => {
-
+  // we should figure out how to paginate this at some point.
   return (dispatch) => {
     dispatch(clearCommentItems());
     dispatch(requestComments());
 
-    xenia().exec('comments_by_user', {user_id})
+    xenia({
+      name: 'comments_by_userid',
+      desc: 'get a reverse chron list of comments'
+    }).addQuery().collection('comments')
+      .match({user_id: `#objid:${user_id}`})
+      .sort(['date_created', -1])
+      .skip(0)
+      .limit(50)
+      // there's a bunch of good stuff here.
+      // we need to do another pass to make the comment list more interesting
+      .include(['body', 'date_created', 'date_updated'])
+      .exec()
       .then(data => {
         dispatch(receiveComments(data));
         dispatch(storeComments(data));

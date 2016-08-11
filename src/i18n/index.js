bc545121bafe62768979bf8a70be839d95a0f967
David Jay <davidgljay@gmail.com>
2016-08-11 15:01:42 -0400

    Stubbing i18n to facilitate testing

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,18 +1,12 @@
 
 /**
- * Module dependencies
+ * Stub internationalization for later replacement
  */
 
-import { Lang } from 'i18n/lang';
-// Lang does not know where did you get your messages from.
-import messages from 'i18n/messages';
-import LangSugar from 'i18n/lang';
-
-const L = new LangSugar();
-
-L.addTranslations(messages['en'], 'en');
-L.addTranslations(messages['de'], 'de');
-L.addTranslations(messages['es'], 'es');
-L.setLocale(L.locale);
+const L = {
+  t:(key) => {
+    return key;
+  }
+};
 
 export default L;

6bfb5fc196fa7315a97b57bbaa4cc1dedaccb248
David Jay <davidgljay@gmail.com>
2016-08-10 20:25:03 -0400

    Testing server responses for getTags

--- test/tags/TagActions.spec.js
+++ test/tags/TagActions.spec.js
@@ -1,20 +1,70 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import {expect} from 'chai';
-// import fetch from 'node-fetch';
 import * as TagActions from '../../src/tags/TagActions';
 
 const middlewares = [thunk]; // add your middlewares like `redux-thunk` 
 const mockStore = configureStore(middlewares);
 
 describe('TagActions', () => {
-  describe('getTags', () => {
-    it('should dispatch a TAG_REQUEST_STARTED action', () => {
-      const getState = {}; // initial state of the store 
 
-      const store = mockStore(getState);
-      TagActions.getTags()(store.dispatch, getState);
-      expect(store.getActions()).to.have.property('type').and.to.equal(TagActions.TAG_REQUEST_STARTED);
+  afterEach(() => {
+    //Reset mock server after each test
+    fetchMock.restore();
+  });
+
+  describe('getTags', () => {
+    let getState;
+    let store;
+
+    beforeEach(() => {
+      // Set the initial state of the store 
+      getState = {
+        app:{
+          pillarHost:'pillarhost'
+        }
+      };
+
+      store = mockStore(getState);
+
+      // Mock a server response
+      fetchMock.mock('pillarhost/api/tags', '{"tags":["tag1","tag2"]}');
+
+    });
+
+    it('should dispatch a TAG_REQUEST_STARTED action', (done) => {
+      TagActions.getTags()(store.dispatch, store.getState)
+        .then(() => {
+          expect(store.getActions()[0]).to.have.property('type')
+            .and.to.equal(TagActions.TAG_REQUEST_STARTED);
+          done();
+        });
+    });
+
+    it ('should dispatch a TAG_REQUEST_SUCCESS action with the retrieved tags on success', (done) => {
+      TagActions.getTags()(store.dispatch, store.getState)
+        .then(() => {
+          let action = store.getActions()[1];
+          expect(action).to.have.property('type')
+            .and.to.equal(TagActions.TAG_REQUEST_SUCCESS);
+          expect(action).to.have.property('payload')
+            .and.to.deep.equal({tags:['tag1','tag2']});
+          done();
+        });
+    });
+
+    it ('should dispatch a TAG_REQUEST_FAILURE action on failure', (done) => {
+      fetchMock.restore();
+      fetchMock.mock('pillarhost/api/tags', {status:404});
+      TagActions.getTags()(store.dispatch, store.getState)
+        .then(() => {
+          let action = store.getActions()[1];
+          expect(action).to.have.property('type')
+            .and.to.equal(TagActions.TAG_REQUEST_FAILURE);
+          expect(action).to.have.property('err')
+            .and.to.equal('404 Not Found');
+          done();
+        });
     });
 
     it('should return method post if not body is given', ()=> {

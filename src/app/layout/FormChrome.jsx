64b30214d48b50892c8f1e2be9747fcf23d2ff69
Dan Zajdband <dan.zajdband@gmail.com>
2016-09-09 10:53:04 -0400

    Contemplate an error while changing the form status

--- src/app/layout/FormChrome.jsx
+++ src/app/layout/FormChrome.jsx
@@ -116,6 +116,7 @@ export default class FormChrome extends React.Component {
   onApplyClick() {
     const { forms, dispatch, activeForm } = this.props;
     const { form, widgets } = forms;
+    console.log('apply click')
     dispatch(saveForm(activeForm ? forms[activeForm] : form, widgets))
       .then(response => {
         this.setState({statusDropdownOpen: false});
@@ -124,6 +125,10 @@ export default class FormChrome extends React.Component {
         } else {
           this.props.dispatch(showFlashMessage('Uh-oh, we can\'t save your form. Try again or report the error to your technical team', 'warning', false));
         }
+      })
+      .catch(err => {
+        this.setState({statusDropdownOpen: false});
+        this.props.dispatch(showFlashMessage('Uh-oh, we can\'t save your form. Try again or report the error to your technical team', 'warning', false));                
       });
   }
 

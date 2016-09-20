f2c9d3813d0ed30a72016a95cd151f4bb71ca55f
Belen Curcio <curciobelen@gmail.com>
2016-09-19 23:57:36 -0300

    Nightwatch config

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,36 +2,36 @@ require('babel-core/register');
 
 module.exports = {
   "src_folders": ["./test/e2e"],
-  "output_folder": "./reports",
-  "page_objects_path": "./test/pages",
-  "globals_path": "test/globals",
+  "output_folder": "reports",
+  "page_objects_path": "pages",
+  "globals_path": "./test/e2e/globals",
   "custom_commands_path" : "",
   "custom_assertions_path" : "",
 
   "selenium": {
-  "start_process": true,
+    "start_process": true,
     "server_path": "./node_modules/selenium-standalone/.selenium/selenium-server/2.53.1-server.jar",
     "log_path": "./nightwatch/reports",
     "host": "127.0.0.1",
-    "port": 4444,
+    "port": 6666,
     "cli_args": {
-    "webdriver.chrome.driver": "./node_modules/selenium-standalone/.selenium/chromedriver/2.24-x64-chromedriver"
-  }
-},
+      "webdriver.chrome.driver": "./node_modules/selenium-standalone/.selenium/chromedriver/2.24-x64-chromedriver"
+    }
+  },
   "test_settings": {
-  "default": {
-    "selenium_port": 4444,
+    "default": {
+      "selenium_port": 4444,
       "selenium_host": "localhost",
       "silent": true,
       "desiredCapabilities": {
-      "browserName": "chrome",
+        "browserName": "chrome",
         "javascriptEnabled": true,
         "acceptSslCerts": true,
         "webStorageEnabled" : true,
         "databaseEnabled" : true,
         "applicationCacheEnabled" : false,
         "nativeEvents" : true
+      }
     }
   }
-}
 };

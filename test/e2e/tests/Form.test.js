0068381d0cc2d6959b31070ef5087612e531fd62
Belen Curcio <curciobelen@gmail.com>
2016-09-20 01:08:52 -0300

    Deleting reports

--- test/e2e/tests/Form.test.js
+++
@@ -1,9 +0,0 @@
-export default {
-  tags: ['modal'],
-  beforeEach: browser => {
-  },
-  'The text that describes what the test is doing': client => {
-    console.log('client = ', client);
-    client.end()
-  },
-};

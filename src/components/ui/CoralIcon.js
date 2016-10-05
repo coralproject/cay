bef340510a80d93fa79d24ae4131d0c58b154f0a
Belen Curcio <curciobelen@gmail.com>
2016-10-05 07:29:59 -0300

    CoralIcon Support Sizes

--- src/components/ui/CoralIcon.js
+++ src/components/ui/CoralIcon.js
@@ -2,16 +2,6 @@ import React, { PropTypes } from 'react';
 
 import { IconButton } from 'react-mdl';
 
-const styles = {
-  base: {
-    color: '#262626',
-    fontSize: '10px'
-  },
-  disabled: {
-    color: '#696969'
-  }
-};
-
 /**
  * Pass as icon the name of the Icon
  * Find them here: https://design.google.com/icons
@@ -22,17 +12,14 @@ const styles = {
  .material-icons.md-24 { font-size: 24px; }
  .material-icons.md-36 { font-size: 36px; }
  .material-icons.md-48 { font-size: 48px; }
+
+ * For more reference: https://github.com/google/material-design-lite/tree/mdl-1.x/src
  */
 
 export const CoralIcon = ({ icon, style, className, onClick, disabled, ...rest}) => (
   <i
-    className={`
-      material-icons md-dark
-      ${disabled ? '.md-inactive' : ''}
-      ${className ? className : ''}
-    `}
-    style={[styles.base, style]}
-    size=""
+    className={`material-icons md-dark md-18 ${disabled ? '.md-inactive' : ''} ${className ? className : ''} `}
+    style={style}
   >
     {icon}
   </i>

9109c8e94e372de6691f50e1fed9a538d9a061a6
Belen Curcio <curciobelen@gmail.com>
2016-10-05 14:53:25 -0300

    CoralTabs Component Added

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,3 +2,5 @@ export { default as CoralIconButton } from './CoralIconButton';
 export { CoralIcon } from './CoralIcon';
 export { default as CoralButton } from './CoralButton';
 export { default as CoralDialog } from './CoralDialog';
+export { default as CoralTabBar } from './CoralTabBar';
+export { default as CoralTab } from './CoralTab';

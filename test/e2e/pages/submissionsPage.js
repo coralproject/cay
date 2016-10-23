b86d800d87e2c00251ec8e6635b784dcec4fbe44
Belen Curcio <curciobelen@gmail.com>
2016-10-23 11:33:31 -0300

    Adding submissionsPage Commands and Elements

--- test/e2e/pages/submissionsPage.js
+++ test/e2e/pages/submissionsPage.js
@@ -2,14 +2,34 @@ const submissionsCommands = {
   ready() {
     return this
       .waitForElementVisible('body', 1000)
+  },
+  sendSubmissionToGallery() {
+    return this
+      .click('@sendToGallery')
+      .waitForElementPresent('@removeFromGallery', 2000);
   }
 }
 
 export default {
   commands: [submissionsCommands],
   elements: {
-    inputEmail: {
-      selector: '#inputEmail'
+    submission: {
+      selector: '.submission'
     },
+    submissionTitle: {
+      selector: '.submission .submission__title h2'
+    },
+    submissionBody: {
+      selector: '.submission .submission__body'
+    },
+    submissionActions: {
+      selector: '.submission .submission__actions'
+    },
+    removeFromGallery: {
+      selector: '.submission .submission__actions .button__remove'
+    },
+    sendToGallery: {
+      selector: '.submission .submission__actions .button__send'
+    }
   }
 }

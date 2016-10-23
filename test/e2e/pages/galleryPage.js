d494c1d77a3e40ed74aa6c6bde41bd0e7ba3cb64
Belen Curcio <curciobelen@gmail.com>
2016-10-23 11:32:20 -0300

    Adding GalleryPage commands

--- test/e2e/pages/galleryPage.js
+++ test/e2e/pages/galleryPage.js
@@ -2,7 +2,26 @@ const galleryCommands = {
   ready() {
     return this
       .waitForElementVisible('body', 1000)
-  }
+  },
+  saveGallery() {
+    return this
+      .click('@formSaveButton')
+      .waitForElementPresent('@statusToggle', 8000)
+      .waitForElementPresent('@flashMessage', 8000)
+  },
+  publishFormOptions() {
+    return this
+      .waitForElementPresent('@formPublishButton', 5000)
+      .click('@formPublishButton')
+  },
+  closeModal() {
+    return this
+      .click('@closeModal')
+  },
+  getGalleryStandaloneUrl(cb) {
+    return this
+      .getValue('@standaloneFormUrl', result => cb({ url: result.value }))
+  },
 }
 
 export default {
@@ -11,5 +30,23 @@ export default {
     inputEmail: {
       selector: '#inputEmail'
     },
+    formPublishButton: {
+      selector: '.form-publish-button'
+    },
+    standaloneFormUrl: {
+      selector: '.standalone-form-url'
+    },
+    closeModal: {
+      selector: ' dialog > div:nth-child(1) > span'
+    },
+    formSaveButton: {
+      selector: '.form-save-button'
+    },
+    statusToggle: {
+      selector: '.form-status-toggle'
+    },
+    flashMessage: {
+      selector: '.flashmessage'
+    }
   }
 }

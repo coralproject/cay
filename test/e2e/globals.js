4d9e550f187ea844032d1dd286a5c11b994c857c
Belen Curcio <curciobelen@gmail.com>
2016-10-23 01:32:02 -0300

    Sending credentials via ENV

--- test/e2e/globals.js
+++ test/e2e/globals.js
@@ -1,4 +1,8 @@
 export default {
   waitForConditionTimeout: 20000,
-  baseUrl: 'http://localhost:3000'
+  baseUrl: 'http://localhost:3000',
+  testUser: {
+    user: process.env.CORAL_TEST_USER,
+    pass: process.env.CORAL_TEST_PASS
+  }
 };

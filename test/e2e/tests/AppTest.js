cbbbb2b9c5fad279598fe98721c1c54c2639a01c
Belen Curcio <curciobelen@gmail.com>
2016-10-24 10:31:55 -0300

    Checking provided hostname

--- test/e2e/tests/AppTest.js
+++ test/e2e/tests/AppTest.js
@@ -1,15 +1,11 @@
-let loginPage;
-
 export default {
-  tags: ['login'],
-  before: client => {
-    const { baseUrl, testUser } = client.globals;
-    loginPage = client.page.loginPage();
-
-  },
-  'User should be redirected': client => {
-    client.pause(1000);
-    client.assert.urlContains('https://auth.coralproject.net/connect');
+  tags: ['app'],
+  'Base url and Hostname': browser => {
+    const { baseUrl } = client.globals;
+    browser
+      .url(baseUrl)
+      .assert.title('Coral - (Beta)')
+      .waitForElementPresent("body", 1000)
   },
   after: client => {
     client.end()

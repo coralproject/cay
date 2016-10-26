28a57c2d9e4d38ade137c61549bae56b9a7ed384
Belen Curcio <curciobelen@gmail.com>
2016-10-26 16:11:31 -0300

    Tooltip Component

--- src/components/ui/Tooltip.js
+++ src/components/ui/Tooltip.js
@@ -1,15 +1,57 @@
-import React, { PropTypes } from 'react';
+import React, { Component,  PropTypes } from 'react';
 
-export const Tooltip = ({ text, for }) => (
-  <span
-    className={`mdl-tooltip`}
-    for={for}
-  >
-    {text}
-  </span>
-);
+export default class Tooltip extends Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    htmlFor: PropTypes.string.isRequired,
+  };
 
-Tooltip.propTypes = {
-  text: PropTypes.string.isRequired,
-  for: PropTypes.isRequired,
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: false
+    };
+
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  handleMouseOver(e) {
+    this.setState({ active: true });
+  }
+
+  handleMouseOut(e) {
+    this.setState({ active: false });
+  }
+
+  render() {
+    const { text, htmlFor, children } = this.props;
+    const { active, x, y } = this.state;
+    return (
+      <span
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+        style={{ position: 'relative'}}
+      >
+        {children}
+        <span
+          className={`mdl-tooltip ${(active) ? 'is-active' : ''}`}
+          style={styles}
+          htmlFor={htmlFor}
+        >
+          {text}
+        </span>
+      </span>
+    );
+  }
+}
+
+const styles = {
+  position: 'absolute',
+  left: 0,
+  top: 'auto',
+  bottom: -50,
 };
+
+

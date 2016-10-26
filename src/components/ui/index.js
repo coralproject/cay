3b4983eb11883542787d35ad6e4480969cfb365c
Belen Curcio <curciobelen@gmail.com>
2016-10-26 16:11:44 -0300

    Tooltip Component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -4,3 +4,4 @@ export { default as CoralButton } from './CoralButton';
 export { default as CoralDialog } from './CoralDialog';
 export { default as CoralTabBar } from './CoralTabBar';
 export { default as CoralTab } from './CoralTab';
+export { default as Tooltip } from './Tooltip';
